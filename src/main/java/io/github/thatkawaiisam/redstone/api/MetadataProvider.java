2edf83ab495db62522a5df8f8358caf55bfe4363
Samuel Sergi <sam.sergi@outlook.com>
2020-06-08 21:36:51 +1000

    Add two comments for the Metadata Providers.

--- src/main/java/io/github/thatkawaiisam/redstone/api/MetadataProvider.java
+++ src/main/java/io/github/thatkawaiisam/redstone/api/MetadataProvider.java
@@ -5,12 +5,14 @@ import com.google.gson.JsonObject;
 public interface MetadataProvider {
 
     /**
+     * Identifier for the Metadata Provider.
      *
      * @return identifier.
      */
     String getID();
 
     /**
+     * Data for the Metadata Provider as a JsonObject.
      *
      * @return constructed data.
      */
